2420702c2e8a054ae78b4b6f793e6122572fff46
macmullensantiago <macmullensantiago@gmail.com>
2022-03-12 12:05:45 -0300

    paypal button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,8 @@ import Title from './Title';
 import CartColumns from './CartColumns';
 import EmptyCart from "./EmptyCart";
 import {ProductConsumer} from '../../context';
-import CartList from './CartList'
+import CartList from './CartList';
+import CartTotals from '.CartTotals';
 export default class Cart extends Component {
     render() {
         return (
@@ -17,6 +18,8 @@ export default class Cart extends Component {
                                     <Title name="your" title="cart"/>
                                     <CartColumns />
                                     <CartList value={value}/>
+                                    <CartTotals value={value} history=
+                                    {this.props.history}/>
                                 </React.Fragment>
                           );
                         } else {
